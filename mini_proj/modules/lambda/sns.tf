279b77c7284148f20a4386bbf5785ad6a375d8f7
archanavadde88 <126779485+archanavadde88@users.noreply.github.com>
2023-03-06 17:35:27 +0000

    Delete mini_proj directory

--- mini_proj/modules/lambda/sns.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sns_topic" "sns" {
-    name = var.name
-}
-resource "aws_sns_topic_subscription" "snstopc" {
-    topic_arn = aws_sns_topic.sns.arn
-    protocol = "lambda"
-    endpoint = aws_lambda_function.test_lambda.arn
-  
-}
--- mini_proj/modules/sqs/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "sqs_output" {
-    value = aws_sqs_queue.queue.arn
-  
-}
