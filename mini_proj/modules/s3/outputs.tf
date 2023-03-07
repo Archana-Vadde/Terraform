d6882589bc94a96b4fd2415151f4d7e8590d334f
archana <archanavadde88@gmail.com>
2023-03-07 19:07:01 +0000

    deleted_unneccode

--- mini_proj/modules/s3/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bucket_arn" {
-    value = aws_s3_bucket.this.arn 
-}
-output "kms_op" {
-    value = aws_kms_key.kms.arn
-  
-}
--- mini_proj/modules/s3/s3_to_sqs_permsn.tf
+++
@@ -1,9 +0,0 @@
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.this.id
-
-  queue {
-    queue_arn     = var.queueoutput
-    events        = ["s3:ObjectCreated:*"]
-  }
-}
