6475b5cee3214d3ccece82a087e3ea185a06f4c9
archana <archanavadde88@gmail.com>
2023-03-17 11:28:55 +0000

    updated_code

--- BestPractice/modules/dynamodb/variables.tf
+++ BestPractice/modules/dynamodb/variables.tf
@@ -3,10 +3,11 @@ variable "create_table" {
   type        = bool
   default     = true
 }
-variable "tags" {
-  description = "Tag map for the resource"
-  type        = map(string)
-  default     = {}
+
+variable "name" {
+  description = "Name of the DynamoDB table"
+  type        = string
+  default     = "demoddb"
 }
 variable "environment" {
   description = "The environment to deploy to."
@@ -18,53 +19,121 @@ variable "environment" {
   }
 }
 
+variable "attributes" {
+  description = "List of nested attribute definitions. Only required for hash_key and range_key attributes. Each attribute has two properties: name - (Required) The name of the attribute, type - (Required) Attribute type, which must be a scalar type: S, N, or B for (S)tring, (N)umber or (B)inary data"
+  type        = list(map(string))
+  default     = [
+    {
+      name : "UserId"
+      type : "S"
+    }
+  ]
+}
 
-# -------------------------------------------
-# DynamoDB Variables
-# -------------------------------------------
-
-variable "db_table_name" {
-  description = "DynamoDB table name"
+variable "hash_key" {
+  description = "The attribute to use as the hash (partition) key. Must also be defined as an attribute"
   type        = string
-  default     = "demo-ddb"
+  default     = "UserId"
+}
+
+variable "range_key" {
+  description = "The attribute to use as the range (sort) key. Must also be defined as an attribute"
+  type        = string
+  default     = null
 }
 
 variable "billing_mode" {
-  description = "DynamoDB billing mode"
+  description = "Controls how you are billed for read/write throughput and how you manage capacity. The valid values are PROVISIONED or PAY_PER_REQUEST"
   type        = string
-  default     = "PAY_PER_REQUEST" # or "PROVISIONED"
-}
-variable "hash_key" {
-  description = "DynamoDB hash key"
-  type        = string
-  default     = "UserId"
+  default     = "PAY_PER_REQUEST"
 }
 
-
-variable "attr_name" {
-  description = "DynamoDB attribute name"
-  type        = string
-  default     = "UserId"
+variable "write_capacity" {
+  description = "The number of write units for this table. If the billing_mode is PROVISIONED, this field should be greater than 0"
+  type        = number
+  default     = null
 }
 
-variable "attr_type" {
-  description = "DynamoDB attribute type"
-  type        = string
-  default     = "S"
+variable "read_capacity" {
+  description = "The number of read units for this table. If the billing_mode is PROVISIONED, this field should be greater than 0"
+  type        = number
+  default     = null
 }
 
-  
+variable "point_in_time_recovery_enabled" {
+  description = "Whether to enable point-in-time recovery"
+  type        = bool
+  default     = false
+}
+
+variable "ttl_enabled" {
+  description = "Indicates whether ttl is enabled"
+  type        = bool
+  default     = false
+}
+
+variable "ttl_attribute_name" {
+  description = "The name of the table attribute to store the TTL timestamp in"
+  type        = string
+  default     = ""
+}
+
+variable "global_secondary_indexes" {
+  description = "Describe a GSI for the table; subject to the normal limits on the number of GSIs, projected attributes, etc."
+  type        = any
+  default     = []
+}
+
+variable "local_secondary_indexes" {
+  description = "Describe an LSI on the table; these can only be allocated at creation so you cannot change this definition after you have created the resource."
+  type        = any
+  default     = []
+}
+
+variable "replica_regions" {
+  description = "Region names for creating replicas for a global DynamoDB table."
+  type        = any
+  default     = []
+}
+
+variable "stream_enabled" {
+  description = "Indicates whether Streams are to be enabled (true) or disabled (false)."
+  type        = bool
+  default     = false
+}
+
+variable "stream_view_type" {
+  description = "When an item in the table is modified, StreamViewType determines what information is written to the table's stream. Valid values are KEYS_ONLY, NEW_IMAGE, OLD_IMAGE, NEW_AND_OLD_IMAGES."
+  type        = string
+  default     = null
+}
 
 variable "server_side_encryption_enabled" {
   description = "Whether or not to enable encryption at rest using an AWS managed KMS customer master key (CMK)"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "server_side_encryption_kms_key_arn" {
   description = "The ARN of the CMK that should be used for the AWS KMS encryption. This attribute should only be specified if the key is different from the default DynamoDB CMK, alias/aws/dynamodb."
   type        = string
-  default     = ""
+  default     = null
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "timeouts" {
+  description = "Updated Terraform resource management timeouts"
+  type        = map(string)
+  default = {
+    create = "10m"
+    update = "60m"
+    delete = "10m"
+  }
 }
 variable "dynamodb_item_create" {
   description = "Controls if DynamoDB table item and associated resources are created"
