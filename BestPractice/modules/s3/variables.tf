af085c7ee8382407616b5ddf03f606c999beed7b
archna94 <126779485+archna94@users.noreply.github.com>
2023-03-27 14:37:57 +0100

    updated_code

--- BestPractice/modules/s3/variables.tf
+++ BestPractice/modules/s3/variables.tf
@@ -1,16 +1,12 @@
 variable "name"{
   type = string
-  default = "demo9876543"
+  default = ""
 }
 variable "create" {
   type = bool
-  default = true
+  default = false
   
 }
-
-  
-
-
 variable "tags" {
   description = "Tag map for the resource"
   type        = map(string)
@@ -18,36 +14,56 @@ variable "tags" {
 }
 variable "environment" {
   type = string
-  default = "dev"
+  default = ""
   
 }
 variable "block_public_acls" {
   description = "Whether Amazon S3 should block public ACLs for this bucket."
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "block_public_policy" {
   description = "Whether Amazon S3 should block public bucket policies for this bucket."
   type        = bool
+  default     = false
+}
+variable "s3_enable_encryption" {
+  description = "Determines whether encryption will be created (affects all resources)"
+  type        = bool
+  default     = false
+}
+variable "s3_kms_master_key_id" {
+  description = "The ID of an AWS-managed customer master key (CMK) for s3"
+  type        = string
+  default = ""
+}
+variable "create_bucket_notification" {
+  description = "Determines whether encryption will be created (affects all resources)"
+  type        = bool
   default     = true
+}
+variable "sqs_arn" {
+  description = "arn of sqs service"
+  type        = string
+  default =  ""
 }
 
 variable "ignore_public_acls" {
   description = "Whether Amazon S3 should ignore public ACLs for this bucket."
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "restrict_public_buckets" {
   description = "Whether Amazon S3 should restrict public bucket policies for this bucket."
   type        = bool
-  default     = true
+  default     = false
 }
 variable "enable_encryption" {
   type        = bool
   description = "Whether or not to use encryption for SNS Topic. If set to `true` and no custom value for KMS key (kms_master_key_id) is provided, it uses the default `alias/aws/sns` KMS key."
-  default     = true
+  default     = false
 }
 variable "kms_master_key_id" {
   description = "The ID of an AWS-managed customer master key (CMK) for Amazon SNS or a custom CMK"
